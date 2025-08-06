ef38b05b05b6537a5bc2d50de41f8f9b65e1d567
Hudson Chung <hudsonch10@gmail.com>
2025-08-06 16:59:39 -0700

    udpated stripe

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,7 +3,7 @@ import { loadStripe } from '@stripe/stripe-js';
 
 // Server-side Stripe instance
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-06-30.basil',
+  apiVersion: '2025-07-30.basil' as any,
 });
 
 // Client-side Stripe instance
